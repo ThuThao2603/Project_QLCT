1ee29d2d7fa5c177849c400654bcb00d791aa515
ThuThao2603 <doanthao26032003@gmail.com>
2024-03-27 08:37:39 +0700

    search

--- app/src/main/java/com/example/btl_qlct/Activity/Activity/TrangChuActivity.java
+++ app/src/main/java/com/example/btl_qlct/Activity/Activity/TrangChuActivity.java
@@ -25,7 +25,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.btl_qlct.Activity.Adapter.CTadapter;
-import com.example.btl_qlct.Activity.Adapter.ChiTieuAdapter;
 import com.example.btl_qlct.Activity.Model.ChiTieuModel;
 import com.example.btl_qlct.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -36,7 +35,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Calendar;
